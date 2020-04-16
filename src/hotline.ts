e0363ded866dd483cec24e999c25b45aed32b8a9
Eric Nichols <underspecified@gmail.com>
2020-04-16 17:54:27 +0900

    added hotline util

---
+++ src/hotline.ts
@@ -0,0 +1,14 @@
+// noinspection JSUnusedGlobalSymbols
+export interface Hotline {
+  pref_ja: string,
+  pref_en: string,
+  center_ja: string,
+  center_en: string,
+  phone: string,
+  lang?: string,
+  hours: string,
+  postal_code?: string,
+  address?: string,
+  url?: string,
+  comments?: string
+}
