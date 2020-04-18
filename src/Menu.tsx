2aa8b881a83ee146f6759877fe5b177ffb95b682
Eric Nichols <underspecified@gmail.com>
2020-04-18 17:56:08 +0900

    Menu: added <div /> container

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -4,36 +4,38 @@ import "./Menu.css"
 
 function Menu() {
   return (
-    <Navbar bg="dark" variant="dark" expand="sm">
-      {/*<Navbar.Brand href="#home">#Covid19HotlinesJp</Navbar.Brand>*/}
+    <div className="menu">
+      <Navbar bg="dark" variant="dark" expand="sm">
+        {/*<Navbar.Brand href="#home">#Covid19HotlinesJp</Navbar.Brand>*/}
 
-      <Nav className="navbar-expand">
-        <Nav.Item>
-          <Nav.Link href="#en" disabled className="EN">EN</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="#jp" disabled className="JP">JP</Nav.Link>
-        </Nav.Item>
-      </Nav>
-
-      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto menu">
+        <Nav className="navbar-expand">
           <Nav.Item>
-            <Nav.Link href="#about" disabled>ABOUT</Nav.Link>
+            <Nav.Link href="#en" disabled className="EN">EN</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#hotlines" disabled>HOTLINES</Nav.Link>
+            <Nav.Link href="#jp" disabled className="JP">JP</Nav.Link>
           </Nav.Item>
-          {/*<Nav.Item>*/}
-          {/*  <Nav.Link href="#news" active>NEWS</Nav.Link>*/}
-          {/*</Nav.Item>*/}
-          {/*<Nav.Item>*/}
-          {/*  <Nav.Link href="#news" active>LINKS</Nav.Link>*/}
-          {/*</Nav.Item>*/}
         </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto menu">
+            <Nav.Item>
+              <Nav.Link href="#about" disabled>ABOUT</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#hotlines" disabled>HOTLINES</Nav.Link>
+            </Nav.Item>
+            {/*<Nav.Item>*/}
+            {/*  <Nav.Link href="#news" active>NEWS</Nav.Link>*/}
+            {/*</Nav.Item>*/}
+            {/*<Nav.Item>*/}
+            {/*  <Nav.Link href="#news" active>LINKS</Nav.Link>*/}
+            {/*</Nav.Item>*/}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   )
 }
 
