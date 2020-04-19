4bd837cd0caa4f15230cb8f255bf2ae31f7e36f1
Eric Nichols <underspecified@gmail.com>
2020-04-19 10:49:45 +0900

    moved Disclaimer to About

--- src/covid19.tsx
+++ src/covid19.tsx
@@ -71,104 +71,10 @@ const makeSymptomsJa = () => {
   )
 }
 
-const makeDisclaimerEn = () => {
-  return (
-    <div className="disclaimer">
-      <h4>Disclaimer</h4>
-        <h5>Accuracy of information on this site</h5>
-        <ul>
-          <li>
-            We try to post information that is as accurate as possible
-            in the content and information on this site. However,
-            information may become outdated.
-          </li>
-          <li>
-            We don’t guarantee accuracy, nor legality or safety.
-          </li>
-        </ul>
-
-        <h5>Liability</h5>
-        <ul>
-          <li>
-            We are not responsible for any damages caused by the contents
-            posted on this site.
-          </li>
-          <li>
-            In addition, if you move from this site to another site by links
-            we posted, we will not take any responsibility for the
-            information and services on those sites.
-          </li>
-          <li>
-            You use the information on this site at your own risk.
-          </li>
-        </ul>
-
-      <h5>Reproduction prohibited</h5>
-        <ul>
-        <li>
-          It is prohibited to reprint the information of copyrighted works
-          such as texts existing on this site for commercial purposes
-          without permission.
-        </li>
-        <li>
-            Please contact us with the URL if you link to this site.
-        </li>
-      </ul>
-    </div>
-  )
-}
-
-const makeDisclaimerJa = () => {
-  return (
-    <div className="disclaimer">
-      <h4>免責事項</h4>
-        <h5>当サイトの情報の正確性について</h5>
-        <ul>
-          <li>
-            当サイトのコンテンツや情報において, 可能な限り正確な情報を掲載するよう努めて
-            いますが 誤情報が入り込んだり, 情報が古くなったりすることもあります。
-          </li>
-          <li>
-            必ずしも正確性を保証するものではありません. また合法性や安全性なども保証
-            しません。
-          </li>
-        </ul>
-
-        <h5>損害等の責任について</h5>
-        <ul>
-          <li>
-            当サイトに掲載された内容によって生じた損害等の一切の責任を負いかねますので,
-            ご了承ください。
-          </li>
-          <li>
-            また当サイトからリンクやバナーなどによって他のサイトに移動された場合,
-            移動先サイトで提供される情報, サービス等について一切の責任も負いません。
-          </li>
-          <li>
-            当サイトを利用する場合は, 自己責任で行う必要があります。
-          </li>
-        </ul>
-
-      <h5>無断転載の禁止について</h5>
-        <ul>
-        <li>
-          当サイトに存在する, 文章や画像, 動画等の著作物の情報を商用目的で無断転載する
-          ことは禁止します。
-        </li>
-        <li>
-          当サイトはリンクフリーですが、リンクを行った場合は、問い合わせ先までリンク
-          元のURLをご連絡ください。
-        </li>
-      </ul>
-    </div>
-  )
-}
-
 const makeCovid19En = () => {
   return (
     <div className="covid19">
       {makeSymptomsEn()}
-      {makeDisclaimerEn()}
     </div>
   )
 }
@@ -177,7 +83,6 @@ const makeCovid19Ja = () => {
   return (
     <div className="covid19">
       {makeSymptomsJa()}
-      {makeDisclaimerJa()}
     </div>
   )
 }
