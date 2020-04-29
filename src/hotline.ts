5ea15111e949466ed5e6eb0518e2810eddf64a90
Eric Nichols <underspecified@gmail.com>
2020-04-29 23:12:25 +0900

    refactoring

--- src/hotline.ts
+++ src/hotline.ts
@@ -1,10 +1,14 @@
+/* eslint-disable */
 import * as A from "fp-ts/lib/Array"
 import * as NEA from "fp-ts/lib/NonEmptyArray"
 import * as O from "fp-ts/lib/Option"
 import * as P from "fp-ts/lib/pipeable"
+// noinspection ES6UnusedImports
+import * as Read from "fp-ts/lib/Reader"
 import * as Rec from "fp-ts/lib/Record"
 import { NonEmptyArray } from "fp-ts/lib/NonEmptyArray"
-//import { Option } from "fp-ts/lib/Option"
+// noinspection ES6UnusedImports
+import { Option } from "fp-ts/lib/Option"
 
 import data from "./data/all.json"
 
@@ -22,6 +26,8 @@ export interface Hotline {
   comments?: string | undefined
 }
 
+type Group<T> = Record<string, NonEmptyArray<T>>
+
 const mergePhone:
   (a: Hotline) => (b: Hotline) => Hotline =
   (a: Hotline) => (b: Hotline) => {
@@ -35,7 +41,6 @@ const mergeLang:
     a.lang = `${a.lang}, ${b.lang}`
     return a
   }
-
 
 const mergeHotlines:
   (a: Hotline) => (b: Hotline) => Array<Hotline> =
@@ -71,39 +76,60 @@ const reduceHotlines:
       O.getOrElse(() => [a, ])
     )
 
-const groupHotlines:
+const collectHotlines:
   (hotlines: NonEmptyArray<Hotline>) => Array<Hotline> =
   (hotlines: NonEmptyArray<Hotline>) =>
-    P.pipe(
-      hotlines,
-      A.reduce([], reduceHotlines),
-    )
+    A.reduce([], reduceHotlines)(hotlines)
+
+const formatStrLabel:
+  (label: string) => (value: string) => string =
+  (label: string) => (value: string) =>
+    `${label}: ${value}`
+
+const formatStrPhone:
+  (phone: string) => string =
+  (phone: string) => {
+    const lbl = phone.startsWith('http') ? "Contact (VoIP)" : "Phone"
+    return formatStrLabel(lbl)(phone)
+  }
+
+const formatStrHours: (_: string) => string =
+  formatStrLabel("Hours of operation")
+
+const formatStrLang: (_: string) => string =
+  formatStrLabel("Supported languages")
 
 const formatStrHotline:
   (h: Hotline) => Array<string> =
   (h: Hotline) =>
-    [`Phone: ${h.phone}`,
-     `Available hours: ${h.hours}`,
-     `Supported languages: ${h.lang}`]
+    [formatStrPhone(h.phone),
+     formatStrHours(h.hours),
+     formatStrLang(h.lang)]
 
 // eslint-disable-next-line
 const A_join:
   (key: string) => (xs: Array<string>) => string =
-  (key: string) => (xs: Array<string>) => xs.join(key)
+  (key: string) => (xs: Array<string>) =>
+    xs.join(key)
+
+// eslint-disable-next-line
+const A_concat:
+  <T>(xs: Array<T>) => (ys: Array<T>) => Array<T> =
+  <T>(xs: Array<T>) => (ys: Array<T>) =>
+    xs.concat(ys)
 
 const groupAndFormatStrHotlines:
   (hotlines: NonEmptyArray<Hotline>) => Array<string> =
   (hotlines: NonEmptyArray<Hotline>) =>
     P.pipe(
-      hotlines,
-      groupHotlines,
+      collectHotlines(hotlines),
       A.map(formatStrHotline),
       A.map(xs => xs.concat(["", ])),
       A.flatten
     )
 
 const groupByCenterJa:
-  (_: NonEmptyArray<Hotline>) => Record<string, NonEmptyArray<Hotline>> =
+  (_: NonEmptyArray<Hotline>) => Group<Hotline> =
     NEA.groupBy((h: Hotline) => h.center_ja)
 
 // fp-ts's implementation doesn't respect key order
@@ -111,8 +137,7 @@ const Rec_collect:
   <A, B>(f: (k: string, a: A) => B) => (r: Record<string, A>) => Array<B> =
   <A, B>(f: (k: string, a: A) => B) => (r: Record<string, A>) =>
     P.pipe(
-      r,
-      Object.entries,
+      Object.entries(r),
       A.map(([k, a]) => f(k, a))
     )
 
@@ -120,21 +145,21 @@ const formatStrCenterName:
   (center: string) => string =
   (center: string) => `## Center: ${center}`
 
-const formatCenter:
-  (centers: Record<string, NonEmptyArray<Hotline>>) => Array<string> =
-  (centers: Record<string, NonEmptyArray<Hotline>>) =>
+const formatStrCenters:
+  (centers: Group<Hotline>) => Array<string> =
+  (centers: Group<Hotline>) =>
     P.pipe(
       centers,
       Rec.map(groupAndFormatStrHotlines),
       Rec_collect((k, a: Array<string>) =>
-        [formatStrCenterName(k), ""].concat(a)
+        [formatStrCenterName(k), ""]
+          .concat(a)
       ),
       A.flatten
     )
 
-const groupStuff:
-  (areas: Record<string, Array<Hotline>>) =>
-    Record<string, Record<string, NonEmptyArray<Hotline>>> =
+const groupPrefs:
+  (areas: Record<string, Array<Hotline>>) => Record<string, Group<Hotline>> =
   (areas: Record<string, Array<Hotline>>) =>
     P.pipe(
       areas,
@@ -146,28 +171,25 @@ const formatStrPrefName:
   (pref: string) => string =
   (pref: string) => `# ${pref}`
 
-const formatStrPref:
-  (prefs: Record<string, Record<string, NonEmptyArray<Hotline>>>) =>
-    Array<string> =
-  (prefs: Record<string, Record<string, NonEmptyArray<Hotline>>>) =>
+const formatStrPrefs:
+  (prefs: Record<string, Group<Hotline>>) => Array<string> =
+  (prefs: Record<string, Group<Hotline>>) =>
     P.pipe(
       prefs,
-      Rec.map(formatCenter),
+      Rec.map(formatStrCenters),
       Rec_collect((k, a: Array<string>) =>
-        [formatStrPrefName(k), ""].concat(a)
+        [formatStrPrefName(k), ""]
+          .concat(a)
       ),
       A.flatten
     )
 
 function main(): void {
-  const grouped =
-    P.pipe(
-      data.area,
-      groupStuff,
-    )
+  const grouped: Record<string, Group<Hotline>> =
+      groupPrefs(data.area)
 
-  const formatted =
-    formatStrPref(grouped)
+  const formatted: Array<string> =
+    formatStrPrefs(grouped)
 
   console.log(formatted.join("\n"))
 }
