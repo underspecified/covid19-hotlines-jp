820dec237adf62ca82c44b25ec25cfded0d675bf
Eric Nichols <underspecified@gmail.com>
2020-05-03 00:40:24 +0900

    center name matches lang setting

--- src/hotline.ts
+++ src/hotline.ts
@@ -28,7 +28,13 @@ export interface Hotline {
   comments?: string | undefined
 }
 
+interface Tx {
+  lang: string,
+  tx: (_: string) => string
+}
+
 type Group<T> = Record<string, NonEmptyArray<T>>
+type WithLang<T> = Reader<string, T>
 
 const mergePhone:
   (a: Hotline) => (b: Hotline) => Hotline =
@@ -59,8 +65,12 @@ const mergeHotlines:
                a.hours === b.hours &&
                b.lang === 'Japanese') {
       return [mergeLang(b)(a),]
+    } else if (a.phone === b.phone &&
+               a.hours === b.hours &&
+               a.lang === b.lang) {
+      return [a,]
     } else {
-      return [a, b]
+       return [a, b]
     }
   }
 
@@ -84,29 +94,31 @@ const collectHotlines:
     A.reduce([], reduceHotlines)(hotlines)
 
 const formatStrLabel:
-  (label: string) => (value: string) => (lang: string) => string =
-  (label: string) => (value: string) => (lang: string) =>
+  (label: string) => (value: string) => WithLang<string> =
+  (label: string) => (value: string) => Read.asks((lang: string) =>
     `${tx(lang)(label)}: ${tx(lang)(value)}`
+  )
 
 const formatStrPhone:
-  (phone: string) => Reader<string, string> =
+  (phone: string) => WithLang<string> =
   (phone: string) => {
     const lbl = phone.startsWith('http') ? "Contact (VoIP)" : "Phone"
     return formatStrLabel(lbl)(phone)
   }
 
-const formatStrHours: (_: string) => (lang: string) => string =
+const formatStrHours: (_: string) => WithLang<string> =
   formatStrLabel("Hours of operation")
 
-const formatStrLang: (_: string) => Reader<string, string> =
+const formatStrLang: (_: string) => WithLang<string> =
   formatStrLabel("Supported languages")
 
 const formatStrHotline:
-  (h: Hotline) => (lang: string) => Array<string> =
-  (h: Hotline) => (lang: string) =>
+  (h: Hotline) => WithLang<Array<string>> =
+  (h: Hotline) => Read.asks( (lang: string) =>
     [formatStrPhone(h.phone)(lang),
      formatStrHours(h.hours)(lang),
      formatStrLang(h.lang)(lang)]
+  )
 
 // eslint-disable-next-line
 const A_join:
@@ -121,21 +133,19 @@ const A_concat:
     xs.concat(ys)
 
 const groupAndFormatStrHotlines:
-  (hotlines: NonEmptyArray<Hotline>) => (lang: string) => Array<string> =
-  (hotlines: NonEmptyArray<Hotline>) => (lang: string) =>
+  (hotlines: NonEmptyArray<Hotline>) => WithLang<Array<string>> =
+  (hotlines: NonEmptyArray<Hotline>) => Read.asks((lang: string) =>
     P.pipe(
       collectHotlines(hotlines),
       A.map(h => formatStrHotline(h)(lang)),
       A.map(xs => xs.concat(["", ])),
       A.flatten
     )
+  )
 
 const groupByCenter:
-  (_: NonEmptyArray<Hotline>) => (lang: string) => Group<Hotline> =
-  (_: NonEmptyArray<Hotline>) => (lang: string) =>
-    NEA.groupBy((h: Hotline) =>
-      lang === "en" ? h.center_en : h.center_ja
-    )(_)
+  (hotlines: NonEmptyArray<Hotline>) => Group<Hotline> =
+    NEA.groupBy((h: Hotline) => h.center_ja)
 
 // fp-ts's implementation doesn't respect key order
 const Rec_collect:
@@ -147,15 +157,40 @@ const Rec_collect:
     )
 
 const formatStrCenterName:
-  (center: string) => (lang: string) => string =
-  (center: string) => (lang: string) =>
-    `## ${formatStrLabel("Center")(center)(lang)}`
+  (center: string) => WithLang<string> =
+  (center: string) =>
+    P.pipe(
+      formatStrLabel("Center")(center),
+      Read.map(x => `## ${x}`)
+    )
+
+const Rec_mapKeys:
+  <A>(f: ([k, a]: [string, A]) => string) => (r: Record<string, A>) =>
+    Record<string, A> =
+  <A>(f: ([k, a]: [string, A]) => string) => (r: Record<string, A>) =>
+      P.pipe(
+        Object.entries(r),
+        A.map(([k, v]) => [f([k, v]), v]),
+        Object.fromEntries
+      )
+
+const getCenterName:
+  (h: Hotline) => Reader<string, string> =
+  (h: Hotline) => Read.asks((lang:string) =>
+    lang === "en" ? h.center_en : h.center_ja
+  )
+
+const updateCenterName:
+  (k: string) => (v: NonEmptyArray<Hotline>) => Reader<string, string> =
+  (k: string) => (v: NonEmptyArray<Hotline>) =>
+    getCenterName(NEA.head(v))
 
 const formatStrCenters:
-  (centers: Group<Hotline>) => (lang: string) => Array<string> =
-  (centers: Group<Hotline>) => (lang: string) =>
+  (centers: Group<Hotline>) => Reader<string, Array<string>> =
+  (centers: Group<Hotline>) => Read.asks((lang: string) =>
     P.pipe(
       centers,
+      Rec_mapKeys(([k, v]) => updateCenterName(k)(v)(lang)),
       Rec.map(hs => groupAndFormatStrHotlines(hs)(lang)),
       Rec_collect((k, a: Array<string>) =>
         [formatStrCenterName(k)(lang), ""]
@@ -163,25 +198,26 @@ const formatStrCenters:
       ),
       A.flatten
     )
+  )
 
 const groupPrefs:
-  (areas: Record<string, Array<Hotline>>) => (lang: string) =>
-    Record<string, Group<Hotline>> =
-  (areas: Record<string, Array<Hotline>>) => (lang: string) =>
+  (areas: Record<string, Array<Hotline>>) => Record<string, Group<Hotline>> =
+  (areas: Record<string, Array<Hotline>>) =>
     P.pipe(
       areas,
       Rec.filterMap(NEA.fromArray),
-      Rec.map(hs => groupByCenter(hs)(lang))
+      Rec.map(groupByCenter)
     )
 
 const formatStrPrefName:
-  (pref: string) => (lang: string) => string =
-  (pref: string) => (lang: string) =>
+  (pref: string) => WithLang<string> =
+  (pref: string) => Read.asks((lang: string) =>
     `# ${tx(lang)(pref)}`
+  )
 
 const formatStrPrefs:
-  (prefs: Record<string, Group<Hotline>>) => (lang: string) => Array<string> =
-  (prefs: Record<string, Group<Hotline>>) => (lang: string) =>
+  (prefs: Record<string, Group<Hotline>>) => WithLang<Array<string>> =
+  (prefs: Record<string, Group<Hotline>>) => Read.asks((lang: string) =>
     P.pipe(
       prefs,
       Rec.map(cs => formatStrCenters(cs)(lang)),
@@ -191,12 +227,13 @@ const formatStrPrefs:
       ),
       A.flatten
     )
+  )
 
 function main(): void {
   const lang = "en"
 
   const grouped: Record<string, Group<Hotline>> =
-      groupPrefs(data.area)(lang)
+    groupPrefs(data.area)
 
   const formatted: Array<string> =
     formatStrPrefs(grouped)(lang)
