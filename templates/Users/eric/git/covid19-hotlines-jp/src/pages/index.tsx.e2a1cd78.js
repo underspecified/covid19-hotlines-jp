37515c5246c83d05f68cd4ba0a672eb4ec0d3d67
Eric Nichols <underspecified@gmail.com>
2020-05-16 14:59:32 +0900

    Updates

--- templates/Users/eric/git/covid19-hotlines-jp/src/pages/index.tsx.e2a1cd78.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{245:function(n,e,a){"use strict";a.r(e);var t=a(0),c=a.n(t),r=a(565);e.default=function(){return c.a.createElement("div",null,c.a.createElement(r.a,{lang:"en"}))}}}]);
--- templates/Users/eric/git/covid19-hotlines-jp/src/pages/jp/support.53c7d6e8.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{251:function(n,e,a){"use strict";a.r(e);var t=a(0),c=a.n(t),r=a(567);e.default=function(){return c.a.createElement("div",null,c.a.createElement(r.a,{lang:"jp"}))}}}]);
