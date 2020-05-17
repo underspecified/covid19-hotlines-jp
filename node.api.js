e42cb23f0819f35dac150eb8d5d31b4dde83e79f
Eric Nichols <underspecified@gmail.com>
2020-05-17 15:21:44 +0900

    fix to chunk naming problems

--- node.api.js
+++ node.api.js
@@ -1,21 +1,24 @@
 exports.default = () => {
   // noinspection JSUnusedLocalSymbols
   return {
-        webpack: function webpack(config, { stage }) {
-            if (typeof(config.entry) === 'string') {
-                config.entry = [
-                  'core-js/stable',
-                  'regenerator-runtime/runtime',
-                  config.entry
-                ];
-            } else {
-                config.entry = [
-                  'core-js/stable',
-                  'regenerator-runtime/runtime',
-                  ...config.entry
-                ];
-            }
-            return config
-        }
+    webpack: function webpack(config, { stage }) {
+      if (typeof(config.entry) === 'string') {
+          config.entry = [
+            'core-js/stable',
+            'regenerator-runtime/runtime',
+            config.entry
+          ];
+      } else {
+          config.entry = [
+            'core-js/stable',
+            'regenerator-runtime/runtime',
+            ...config.entry
+          ];
+      }
+
+      config.optimization.splitChunks.name = false
+
+      return config
     }
+  }
 }
